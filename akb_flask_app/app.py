afc7dbd9d7db1da92fc270c66d85c3856683ea78
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2025-10-21 15:40:02 +0630

    update

---
+++ akb_flask_app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/github")
+def github():
+    return render_template("github.html")
+
+
+
+
+app.run(debug=True)
