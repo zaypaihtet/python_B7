3cb5a4dcb4b88d673b1fffb77c002e9af2951ed9
Hannahlene <kslpasherah@gmail.com>
2025-10-28 14:35:24 +0630

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   db.sql
    	new file:   templates/add_project.html
    	new file:   templates/admin_projects.html
    	new file:   templates/edit_project.html
    	new file:   templates/message.html

--- db.sql
+++ db.sql
@@ -14,3 +14,11 @@ CREATE TABLE admin (
 );
 
 INSERT INTO admin (username, password) VALUES ('admin', 'admin123');
+
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    image VARCHAR(255),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
