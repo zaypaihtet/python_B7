bac3c10ecd0f9d0a1fed3c887496d3395df96c33
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2025-11-06 16:11:29 +0630

    separate routes from app.py

--- akb_flask_app/app.py
+++ akb_flask_app/app.py
@@ -1,23 +1,8 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/github")
-def github():
-    return render_template("github.html")
+from app import create_app
 
 
+app = create_app()
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
